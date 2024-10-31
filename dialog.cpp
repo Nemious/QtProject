750491270bcc56ea37e0a9d32c922ffcfeaf4497
Nemious <gracesholmes66@gmail.com>
2024-10-31 18:39:00 +0500

    Конечные изменения

--- dialog.cpp
+++ dialog.cpp
@@ -1,6 +1,7 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include "primelist.h"
+#include <QMessageBox>
 
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
@@ -17,11 +18,20 @@ Dialog::~Dialog()
 
 void Dialog::calculate() {
     qDebug() << "Function called: " << Q_FUNC_INFO;
-    int a = ui->lineEditA->text().toInt();
-    int b = ui->lineEditB->text().toInt();
+    bool isAOK, isBOK;
+    int a = ui->lineEditA->text().toInt(&isAOK);
+    int b = ui->lineEditB->text().toInt(&isBOK);
+
+    if (!isAOK) {
+        QMessageBox::critical(this, "Ошибка", "A должно быть числом");
+        return; // Прерывание выполнения функции
+    }
+    if (!isBOK) {
+        QMessageBox::critical(this, "Ошибка", "B должно быть числом");
+        return; // Прерывание выполнения функции
+    }
 
     ui->listWidget->clear();
-
 
     for (int n : getPrimes(a, b)) {
         ui->listWidget->addItem(QString::number(n));
